969d1a90d1ea1c2821aa796098e281bd2704b760
EC2 Default User <ec2-user@ip-172-31-27-35.eu-west-1.compute.internal>
2019-08-09 09:26:08 +0000

    CRUD finished

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ app.set('view engine','ejs');// Set the template engine
 
 app.use(express.static("views")); // Allow access to views folder
 app.use(express.static("style")); // Allow access to styling folder
-app.use(express.static("images"));
+app.use(express.static("images")); // Allow access to images
 
 // body parser to get information
 var fs = require('fs')
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser") // Call body parser modul and make use of it
 app.use(bodyParser.urlencoded({extended:true}));
 
+// ******************************************From here is JSON data ********************************
 var contact = require("./model/contact.json");
 
 // set up simple hello world application using the request and response function
@@ -27,9 +28,9 @@ app.get('/contacts', function(req,res){
     console.log("I found the contacts page");
 });
 
-// set up simple hello world application using the request and response function
+// Get the contact us page
 app.get('/add', function(req, res) {
-res.render("add"); // we set the response to send back the string hello world
+res.render("add"); // we set the response to send back the string I found the contact us page
 console.log("I found the contact us page"); // used to output activity in the console
 });
 
@@ -54,7 +55,7 @@ app.post("/add",function(req,res){
     
     var newId = maxCid.id + 1; // add 1 to old largest to make new largest
     // show the result in the console
-    console.log("new Id is" + newId)
+    console.log("new Id is " + newId)
     // we need to get access to what the user types in the form
     // and pass it to our JSON file as the new data
     
@@ -62,7 +63,7 @@ app.post("/add",function(req,res){
         id: newId,
         name: req.body.name,
         comment: req.body.comment,
-        email: req.body.eamil
+        email: req.body.email
         
     }
     fs.readFile('./model/contact.json','utf8', function readfileCallback(err){
@@ -72,7 +73,7 @@ app.post("/add",function(req,res){
             
             contact.push(contactsx); // add the new data to the JSON file
             json = JSON.stringify(contact, null, 4); // this line structures the JSON so it's easy on the eye
-            fs.writeFile('./model/contact.json',json,'utf8')
+            fs.writeFile('./model/contact.json',json, 'utf8')
         }
     })
     
@@ -81,6 +82,77 @@ app.post("/add",function(req,res){
     
 });
 
+// Now we code for the edit JSON date
+
+// *** get page to edit 
+app.get('/editcontact/:id', function(req,res){
+    // Now we build the actual information based on the changes made by the user 
+   function chooseContact(indOne){
+       return indOne.id === parseInt(req.params.id)
+       }
+
+
+  var indOne = contact.filter(chooseContact)
+    
+   res.render('editcontact', {res:indOne}); 
+    
+});
+
+// *** Perform the edit
+
+ app.post('/editcontact/:id', function(req,res){
+     
+     //firstly we need to stringify our JSON data so it can be call as a variable an modified as needed
+     var json = JSON.stringify(contact)
+     
+     // declare the incoming id from the url as a variable
+     var keyToFind = parseInt(req.params.id)
+    
+    // use predetermined JavaScript functionality to map the data and find the Information i need
+    var index = contact.map(function(contact) {return contact.id}).indexOf(keyToFind)
+    
+    // the next three lines get the content from the body where the user fills in the form 
+    var z = parseInt(req.params.id);
+    var x = req.body.name   
+    var y = req.body.comment
+    // The next section pushes the new data into the json file in place of the data to be updated
+    contact.splice(index, 1, {name: x, comment: y, email: req.body.email, id: z })
+    
+    
+    // Now we reformat the JSON and push it back to the actual file 
+    json = JSON.stringify(contact, null, 4); // this line structures the JSON so it's easy on the eye
+    fs.writeFile('./model/contact.json',json,'utf8',function(){})
+    
+    res.redirect("/contacts");
+})
+
+app.get ('/deletecontact/:id', function(req,res){
+    
+    
+    
+    // firstly we need to stringify our JSON data so it can be call as a variable an modified as needed
+    var json = JSON.stringify(contact)
+    
+    // declare the incoming id from the url as a variable 
+    var keyToFind = parseInt(req.params.id)
+    
+    // use predetermined JavaScript functionality to map the data and find the information I need 
+    var index = contact.map(function(contact) {return contact.id}).indexOf(keyToFind)
+    
+
+    contact.splice(index, 1)
+    
+  
+    
+    // now we reformat the JSON and push it back to the actual file
+    json = JSON.stringify(contact, null, 4); // this line structures the JSON so it is easy on the eye
+    fs.writeFile('./model/contact.json',json, 'utf8', function(){})
+    
+    res.redirect("/contacts");
+    
+    
+    
+})
 
 
 // this code provides the server port for our application to run on
